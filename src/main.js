00382efd57d207d9080e8cf32390b8ca2ef3f261
LL\guscasti <gustavo.castillo@prodigious.cr>
2017-09-15 18:26:24 -0600

    my test

---
+++ src/main.js
@@ -0,0 +1,25 @@
+var mainModule = {
+    init: function () {
+        this.overlyProgressBar.init();  
+    },
+    overlyProgressBar: {
+    progressBar:'.progress .progress-bar',
+    skill:'.skill',
+        init: function () {
+            var me = this;
+
+
+           $(me.progressBar).animate({width:  $(me.progressBar).attr("aria-valuenow") + "%"}, 2500);
+
+            $(me.skill).css("left",
+                function () {
+                    return  $(me.progressBar).attr("aria-valuenow") + "%";
+                }
+            )
+        }
+    }
+}
+
+$(function () {
+	mainModule.init();
+});
